51d30064beb1c7b53412afb496b04556ebc00ab7
Raphaël <150128219+Aomigo@users.noreply.github.com>
2024-09-23 14:59:32 +0200

    Update loot.js

--- assets/js/loot.js
+++ assets/js/loot.js
@@ -1,25 +1,51 @@
 function loot(Expedition) {
     damage(Expedition)
-    resultLoot()
     if (Expedition.textContent.includes("Cave")) CaveLoot()
     if (Expedition.textContent.includes("Meadow")) MeadowLoot()
     if (Expedition.textContent.includes("Raid")) RaidLoot()
     if (Expedition.textContent.includes("Underwater")) UnderwaterLoot()
 
     function CaveLoot() {
-
+        let result = Math.floor(Math.random() * (4 - 1) + 1)
+        let loot = []
+        for(let i = 0; i < result; i++) {
+            loot.push(ingredients[Math.floor(Math.random() * (12 - 0) + 0)])
+        }
+        console.log(result)
+        console.log(loot)
+        artefacts()
+        resultLoot(result)
     }
 
     function MeadowLoot() {
-
+        let result = Math.floor(Math.random() * (3 - 1) + 1)
+        let loot = []
+        for(let i = 0; i < result; i++) {
+            loot.push(ingredients[Math.floor(Math.random() * (12 - 0) + 0)])
+        }
+        console.log(result)
+        console.log(loot)
+        artefacts()
+        resultLoot(result)
     }
 
     function RaidLoot() {
-
+        let result = Math.floor(Math.random() * (7 - 2) + 1)
+        let loot = []
+        for(let i = 0; i < result; i++) {
+            loot.push(ingredients[Math.floor(Math.random() * (12 - 0) + 0)])
+        }
+        console.log(result)
+        console.log(loot)
+        artefacts()
+        resultLoot(result)
     }
 
     function UnderwaterLoot() {
+        let loot = ["Fish", "Water"]
 
+        console.log(loot)
+        resultLoot(2)
     }
 }
 
@@ -35,7 +61,7 @@ function damage(Expedition) {
         TotalExhaustion -= Math.floor(Math.random() * (15 - 10) + 10)
         TotalHunger -= Math.floor(Math.random() * (15 - 10) + 10)
         damageDeduction()
-        
+
     }
 
     function MeadowDamage() {
@@ -66,7 +92,7 @@ function damage(Expedition) {
         if (TotalHealth < 0) TotalHealth = 0
         healthBar.style.width = TotalHealth + "%"
         setTimeout(() => {
-           DmgHealth()
+            DmgHealth()
         }, 700)
 
         function DmgHealth() {
@@ -96,6 +122,7 @@ function damage(Expedition) {
             exhaustionWrapper.style.width = TotalExhaustion + "%"
         }
         //happiness
+        if (TotalHapiness > 100) TotalHapiness = 100
         hapinessWrapper.style.width = TotalHapiness + "%"
         setTimeout(() => {
             HealHapiness()
@@ -117,7 +144,38 @@ function damage(Expedition) {
     }
 }
 
-
-function resultLoot() {
-    
+//en fonction du loot changer le text
+function resultLoot(loot) {
+    if(loot == 0) LootScrollerResults.innerHTML = "The looting was... lets not talk about that."
+    if(loot <= 2 && loot !== 0) LootScrollerResults.innerHTML = "The looting was not very good."
+    if(loot == 3) LootScrollerResults.innerHTML = "The looting was successful."
+    if(loot > 3) LootScrollerResults.innerHTML = "The looting was excellent !"
 }
+
+function artefacts() {
+    let results = Math.floor(Math.random() * (100 - 1) + 1)
+    if(results == 1) {
+        artefactFound()
+    }
+
+    function artefactFound() {
+        let artefactJustFound = Artefacts[Math.floor(Math.random() * (5 - 0) + 0)]
+        console.log("you found " + artefactJustFound + "...")
+    }
+}
+
+LootscrollerButton.addEventListener('click', function () {
+    if (LootScroller.classList.contains('hidden')) {
+        console.log("ya")
+        LootScroller.classList.add('appear')
+        LootScroller.classList.remove('dissapear')
+        LootScroller.classList.remove('hidden')
+    } else {
+        console.log("nein")
+        LootScroller.classList.remove('appear')
+        LootScroller.classList.add('dissapear')
+        setTimeout(() => {
+            LootScroller.classList.add('hidden')
+        }, 500)
+    }
+})
